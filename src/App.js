efba7004762a9ff17d27032062a84ae3bffa6c63
Adnan Abubakar Adams <adnan_abubakaradams@yahoo.com>
2022-05-17 20:23:54 -0400

    birdsSancturary passing all test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import BirdsCards from "./Components/BirdsCards";
+import Checkout from "./Components/Checkout";
+import Cart from "./Components/Cart";
+import { useState } from "react";
+import birdData from "./data/birds";
+import "./App.css";
+
 const App = () => {
+  // const [adopt, setAdopt] = useState("");
+
+  const [adopt, setAdopt] = useState([]); // destructuring the state at which the click of a button will add the name of the bird to the cart
+  const [price, setPrice] = useState([]); // destructuring the state at which the click of a button will add the price of the bird to the cart
+  // followed by importing components Cart, BirdsCards and Checkout and passing props into them!!
   return (
     <div>
-      <h1>Hello, world!</h1>
+      {/* <h1>Hello, world!</h1> */}
+      <Cart adopt={adopt} price={price}/> 
+      <BirdsCards birds={birdData} setAdopt={setAdopt} setPrice={setPrice} /> 
+      <Checkout setAdopt={setAdopt} setPrice={setPrice}/>
     </div>
   );
 };
---
+++ src/Components/Checkout.js
@@ -0,0 +1,82 @@
+import React from "react";
+import { useState } from "react";
+import Cart from "./Cart";
+
+// const firstName = document.querySelector("#FirstName");
+
+const Checkout = (props) => {
+  const { setAdopt, setPrice } = props;
+
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [zipCode, setZipCode] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (!firstName || !lastName || !email || !zipCode) {
+      alert("Please complete form");
+    } else {
+      alert("You have adopted birds. Thank you!");
+
+      setAdopt([]);
+      setPrice([]);
+      setEmail("");
+      setFirstName("");
+      setLastName("");
+      setZipCode("");
+
+      event.target.reset();
+    }
+  };
+  return (
+    <div className="Checkout">
+      <form onSubmit={handleSubmit}>
+        <label>
+          First Name
+          <br />
+          <input 
+          id="FirstName" 
+          name="FirstName" 
+          type="text" 
+          value={firstName}
+          onChange={(event) => setFirstName(event.target.value)}/>
+        </label>
+        {/* {console.log(firstName.value)} */}
+        <label>
+          Last Name
+          <br />
+          <input 
+          id="LastName" 
+          name="LastName" 
+          type="text" 
+          value={lastName}
+          onChange={(event) => setLastName(event.target.value)}/>
+        </label>
+        <label>
+          Email
+          <br />
+          <input 
+          id="Email" 
+          name="Email" 
+          type="email" 
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}/>
+        </label>
+        <label>
+          Zip Code
+          <br />
+          <input 
+          id="ZipCode" 
+          name="ZipCode" 
+          type="number" 
+          value={zipCode} 
+          onChange={(event) => setZipCode(event.target.value)}/>
+        </label>
+        <input label="Submit" type="submit"></input>
+      </form>
+    </div>
+  );
+};
+
+export default Checkout;
